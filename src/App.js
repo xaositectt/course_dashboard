0cf272412343a4f38a6ab1a90ba9deb1fd24eb8d
xaositectt <xaositectt@gmail.com>
2020-06-01 19:36:48 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HelloWorld from './components/hello_world'
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
